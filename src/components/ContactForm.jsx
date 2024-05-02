3cd1b8db34e1867e8df7b63a8a61917b5f6dbe12
Alejandrolt <alejandrolt54@gmail.com>
2024-05-01 19:21:32 -0500

    taskreact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -42,6 +42,7 @@ function ContactForm() {
       <label>
         Name:
         <input
+          style={{ marginBottom: '9px' }}
           type="text"
           name="name"
           value={name}
